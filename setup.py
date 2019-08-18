a053597b5712e6344a8ae176fe100d64c559f192
Jay Shah <jrshah01@gmail.com>
2019-08-18 22:19:20 +0100

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='FootballMatchdaySummarizer',
     version='1.0.0',
     description='Summarizes Football Match Days',
-    install_requires=['python = 3',
+    install_requires=['python >= 3',
                       'enaml',
                       'atom',
                       ],
