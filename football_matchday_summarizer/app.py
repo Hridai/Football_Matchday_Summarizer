cd81b6470df06c1a67f5c8105dc6ec59ba5193be
Jay Shah <jrshah01@gmail.com>
2019-08-18 23:32:40 +0100

    change import

--- football_matchday_summarizer/app.py
+++ football_matchday_summarizer/app.py
@@ -5,9 +5,9 @@ from enaml.qt.qt_application import QtApplication
 def main():
     import pdb; pdb.set_trace()
     with enaml.imports():
-        from football_matchday_summarizer.view import Main
+        from football_matchday_summarizer import view
     app = QtApplication()
-    view = Main()
+    view = view.Main()
     view.show()
     app.start()
 
