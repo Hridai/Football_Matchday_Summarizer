bfb3adc7d2780bb1ee4748e7c3159461c0845e89
Jay Shah <jrshah01@gmail.com>
2019-08-18 23:35:01 +0100

    trying relative import

--- football_matchday_summarizer/app.py
+++ football_matchday_summarizer/app.py
@@ -5,7 +5,7 @@ from enaml.qt.qt_application import QtApplication
 def main():
     import pdb; pdb.set_trace()
     with enaml.imports():
-        from football_matchday_summarizer import view
+        from . import view
     app = QtApplication()
     view = view.Main()
     view.show()
