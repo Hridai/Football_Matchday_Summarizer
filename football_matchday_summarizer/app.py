d3c6e41be0b182a259952838a106c638e49076d7
Jay Shah <jrshah01@gmail.com>
2019-08-18 23:46:03 +0100

    fix import

--- football_matchday_summarizer/app.py
+++ football_matchday_summarizer/app.py
@@ -5,7 +5,7 @@ from enaml.qt.qt_application import QtApplication
 
 def main():
     with enaml.imports():
-        from football_matchday_summarizer import Main
+        from football_matchday_summarizer.view import Main
     app = QtApplication()
     view = Main()
     view.show()
