a111eaab5dd48127f86832cdc2a57ec064bbee8f
Jay Shah <jrshah01@gmail.com>
2019-08-18 23:47:20 +0100

    rename package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='somethingrandom',
+    name='football_matchday_summarizer',
     version='1.0.0',
     description='Summarizes Football Match Days',
     install_requires=['enaml',
@@ -12,6 +12,6 @@ setup(
     packages=find_packages(),
     package_data={'football_matchday_summarizer': ['*.enaml']},
     entry_points={'console_scripts': [
-        'app_run = football_matchday_summarizer.app:main',
+        'run-app = football_matchday_summarizer.app:main',
     ]}
 )
